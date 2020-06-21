4471a6a9a742a61a4003df0227ba5ac78ee0c752
radpi42 <67181776+radpi42@users.noreply.github.com>
2020-06-21 02:32:52 -0700

    new version of primary code

--- resetbutton.py
+++ resetbutton.py
@@ -1,4 +1,4 @@
-from gpiozero import Button, LED, PWMLED
+from gpiozero import Button, LED
 from subprocess import check_call
 from signal import pause
 from time import sleep
@@ -18,8 +18,8 @@ red = LED(20)
 #set button gpio pins
 
 Button.was_held = False
-shutdown_btn = Button(2, hold_time=3)
-restart_btn = Button(16, hold_time=2)
+shutdown_btn = Button(2, hold_time=1.25)
+restart_btn = Button(16, hold_time=1.25)
 
 #functions for shutdown
 
@@ -36,8 +36,8 @@ def SDheld(shutdown_btn):
     red.off()
     shutdown_btn.was_held = True
     red.on()
-    sleep(2)
- #   print("Shutdown Process Armed")
+    sleep(1.25)
+#    print("Shutdown Process Armed")
     red.blink(.25, .25)
     pause()
         
@@ -47,14 +47,16 @@ def shutdown():
     if shutdown_btn.was_held:
   #      print("Shutdown Process Intialized")
         sleep(1)
-        red.pulse()
-        sleep(3)
+        red.off()
+        sleep(1.25)
+        red.on()
    #     print("Shutting Down")
         sleep(1)
     #    print("Until Next Time!")
         red.off()
         shutdown_btn.was_held = False
-        check_call(['sudo', 'poweroff'])
+        print("success")
+#####        check_call(['sudo', 'poweroff'])
     else:
         red.off()
      #   print("Cancled")
@@ -75,7 +77,7 @@ def RBheld(reboot_btn):
     reboot_btn.was_held = True
     blue.off()
     blue.on()
-    sleep(2)
+    sleep(1)
   #  print("Reboot Process Armed")
     blue.blink(.25, .25)
            
@@ -83,18 +85,21 @@ def RBheld(reboot_btn):
 def reboot():
     if restart_btn.was_held:
    #     print("Reboot Process Intialized")
-        sleep(2)
-        blue.pulse()
-        sleep(3)
-    #    print("Rebooting")
         sleep(1)
+        blue.off()
+        sleep(1.5)
+        blue.on()
+    #    print("Rebooting")
+        sleep(1.25)
      #   print("See you soon!")
         blue.off()
         restart_btn.was_held = False
-        check_call(['sudo', 'reboot'])    
+        print("success")
+#####        check_call(['sudo', 'reboot'])    
     else:
         blue.off()
         sleep(0.5)
+        restart_btn.was_held = False
       #  print("Cancled")
         
       
@@ -111,6 +116,3 @@ while True:
     restart_btn.when_pressed = RBpressed
     restart_btn.when_held = RBheld
     restart_btn.when_released = reboot
-
-
-   
